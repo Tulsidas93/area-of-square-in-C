2a3878841422d14fd01bc4f2b5ef21f4d16bad7c
Tulsidas Takekar <156583606+Tulsidas93@users.noreply.github.com>
2024-01-29 19:46:43 +0530

    areaofsquare.c
    calculating area of square intitial code

---
+++ areaofsquare.c
@@ -0,0 +1,13 @@
+#include<stdio.h>                    //header file decleration
+int main()                              
+{
+    int side,area;                   //veriabel declearation
+printf("enter value of sides:");     //print "Statement"
+scanf("%d",&side);                   // take input for keypad as side value
+printf("side is:%d",side);           //print input value
+area=side*side;                      // calculate area
+printf("area of square is:%d",area); // print value of area.
+return 0;                  
+
+
+}
